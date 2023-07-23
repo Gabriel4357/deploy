e203ff33e335e8721f6e988acc87a793f459910a
Gabriel <gabrielseethor.jh@gmail.com>
2023-07-24 01:53:32 +0800

    test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController');
+const { Router } = require("express");
+const authController = require("../controllers/authController");
 
 const router = Router();
 
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
+router.get("/login", authController.login_get);
+router.post("/login", authController.login_post);
 
 module.exports = router;
